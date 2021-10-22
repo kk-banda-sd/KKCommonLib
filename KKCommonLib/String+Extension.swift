2042572a7ddb05c138d067e68837450649c57c0f
kk-cps <64898911+kk-banda-sd@users.noreply.github.com>
2021-10-22 15:11:43 +0300

    initial commit

---
+++ KKCommonLib/String+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extension.swift
+//  KKCommonLib
+//
+//  Created by Kostya Karakay on 22.10.2021.
+//
+
+import Foundation
+
+extension String {
+    var notEmpty: Bool {
+        return !self.isEmpty
+    }
+}
