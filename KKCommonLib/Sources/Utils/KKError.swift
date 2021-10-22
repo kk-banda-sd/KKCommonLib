5b4f2b13a5ad5ed299880b3d3e393d6e8ce7e0b6
kk-cps <64898911+kk-banda-sd@users.noreply.github.com>
2021-10-23 01:57:38 +0300

    commit

---
+++ KKCommonLib/Sources/Utils/KKError.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public class KKError {
+    
+    // MARK: - Variables
+    public var message: String = ""
+    
+    public var code: Int = 0
+    
+    // MARK: - Helpers
+    public var error: Error {
+        return NSError(domain: "domain", code: code, userInfo: [NSLocalizedDescriptionKey : message])
+    }
+}
+
+// MARK: - Initialization
+public extension KKError {
+    convenience init(_ error: Error) {
+        self.init(message: error.localizedDescription, code: (error as NSError).code)
+    }
+    
+    convenience init(message: String, code: Int = 0) {
+        self.init(message: message, code: code)
+    }
+    
+    static var internalServer: KKError {
+        return KKError(message: "Internal server error", code: 500)
+    }
+}
+
+public extension Error {
+    var kkError: KKError {
+        return KKError(self)
+    }
+}
